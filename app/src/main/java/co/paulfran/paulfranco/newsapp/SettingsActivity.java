16107be201e43dce05303ac9e4b2aa43735820f2
Paul Franco <paulfranco@me.com>
2018-06-11 02:33:19 -0700

    crated the menu resource directory and the main resource file

--- app/src/main/java/co/paulfran/paulfranco/newsapp/SettingsActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/newsapp/SettingsActivity.java
@@ -1,4 +1,17 @@
 package co.paulfran.paulfranco.newsapp;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class NewsPreferenceFragment extends PreferenceFragment {
+
+    }
 }
